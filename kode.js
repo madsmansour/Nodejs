6d1e936dd87221c6ca8e69e0eb4533cce696f613
madsmansour <42993941+madsmansour@users.noreply.github.com>
2019-10-07 14:33:52 +0200

    go

--- kode.js
+++ kode.js
@@ -1,11 +1,22 @@
 var http = require('http');
 var date = require('./dato');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     var studerende = {};
     studerende.navn = "Cosby";
     studerende.humør = false;
-    res.end(JSON.stringify(studerende) + "</br>" + date.myDateTime());
+    studerende.tid = date.myDateTime();
+    fs.readFile("dogpoop.png");
+    res.end(JSON.stringify(studerende) + fs);
 }).listen(999);
 
+
+
+/*
+fs.appendFile('demofile1.html', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+*/
