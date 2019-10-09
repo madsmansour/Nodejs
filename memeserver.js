7a639897329533eac62553bcc0362643665529cd
madsmansour <42993941+madsmansour@users.noreply.github.com>
2019-10-09 11:17:17 +0200

    123

--- memeserver.js
+++ memeserver.js
@@ -3,7 +3,6 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const router = require('./displayImage/router');
-
 const fs = require("fs")
  
 const directoryPath = path.join(__dirname, "displayImage/memes/")
@@ -23,29 +22,24 @@ fs.readdir(directoryPath, function(err, files) {
 
         // et for-loop der løber hvert element igennem og oprette det som en unordered list.
     for (let file of files) {
-        html += "<h1>";
+        html += "<h1 style=color:red><strong>";
         html += "<li>";
-        html += `<a href="/memes/${file}">Billede ${number}  ${file}</a>`;
+        html += `<a href="/memes/${file}">Meme nummer ${number} = ${file} 💩</a>`;
         html += "</li>";
+        html += "</h1></strong>";
         number++;
     }
         html += "</ul>";
-        html += "</h1>";
-
+  
+// bruger min express variabel app til at sende min html variabel
     app.get('/', (req, res) => res.send(html));
 
   }
 })
 
 
-
-
-
-
 app.use('/gif', router);
 app.use(express.static(path.join(__dirname, 'displayImage')));
-
-
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
