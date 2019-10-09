2f518fae5a7c3b7836744c56c7c03e9e8ec6c7c6
madsmansour <42993941+madsmansour@users.noreply.github.com>
2019-10-09 09:16:27 +0200

    j

--- displayImage/controller.js
+++ displayImage/controller.js
@@ -3,4 +3,6 @@ exports.index = function(req, res){
     var imgID = req.params.id;
 
     res.send(`<img src="/memes/${imgID}.png"></img>`);
-}//C:/Users/Mads/Desktop/Web development/Node/displayImage/memes
+}
+
+//C:/Users/Mads/Desktop/Web development/Node/displayImage/memes
