3b7800cde6fcb9369c314b5386a13ac820330bde
madsmansour <42993941+madsmansour@users.noreply.github.com>
2019-10-09 11:29:47 +0200

    formattet

--- memeserver.js
+++ memeserver.js
@@ -4,37 +4,37 @@ const port = 3000;
 const path = require('path');
 const router = require('./displayImage/router');
 const fs = require("fs")
- 
+
 const directoryPath = path.join(__dirname, "displayImage/memes/")
 
 
 // bruger fs.readdir til at gennemløbe min mappe /memes/
-fs.readdir(directoryPath, function(err, files) {
-  
-  if (err) {
-    console.log("Error getting directory information.")
-  } 
-  
-  else {
+fs.readdir(directoryPath, function (err, files) {
+
+    if (err) {
+        console.log("Error getting directory information.")
+    }
+
+    else {
 
         let html = "<ul>";
         let number = 1;
 
         // et for-loop der løber hvert element igennem og oprette det som en unordered list.
-    for (let file of files) {
-        html += "<h1 style=color:red><strong>";
-        html += "<li>";
-        html += `<a href="/memes/${file}">Meme nummer ${number} = ${file} 💩</a>`;
-        html += "</li>";
-        html += "</h1></strong>";
-        number++;
-    }
+        for (let file of files) {
+            html += "<h1 style=color:red><strong>";
+            html += "<li>";
+            html += `<a href="/memes/${file}">Meme nummer ${number} = ${file} 💩</a>`;
+            html += "</li>";
+            html += "</h1></strong>";
+            number++;
+        }
         html += "</ul>";
-  
-// bruger min express variabel app til at sende min html variabel
-    app.get('/', (req, res) => res.send(html));
 
-  }
+        // bruger min express variabel app til at sende min html variabel
+        app.get('/', (req, res) => res.send(html));
+
+    }
 })
 
 
